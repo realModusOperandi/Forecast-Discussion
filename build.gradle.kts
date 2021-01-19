0cffd17a2dd9a228e3de2a9771e5098672695d1f
realModusOperandi <liamwestby@icloud.com>
2021-01-18 18:43:44 -0600

    Update Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-import net.wasdev.wlp.gradle.plugins.extensions.FeatureExtension
-import net.wasdev.wlp.gradle.plugins.extensions.ServerExtension
+import io.openliberty.tools.gradle.extensions.ServerExtension
 import kotlin.collections.mapOf
 
 plugins {
-    kotlin("jvm") version "1.3.72"
-    id("net.wasdev.wlp.gradle.plugins.Liberty")  version "2.6.5"
+    kotlin("jvm") version "1.4.21"
+    id("io.openliberty.tools.gradle.Liberty") version "3.1.1"
     war
 }
 
@@ -13,13 +12,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
     providedCompile("javax", "javaee-api", "8.0")
 
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.2")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.2")
 
-    libertyRuntime("io.openliberty", "openliberty-runtime", "[20.0.0.3,)")
+    libertyRuntime("io.openliberty", "openliberty-runtime", "[20.0.0.12,)")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
@@ -33,14 +31,13 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-val httpPort by extra { 9080 }
-val httpsPort by extra { 9443 }
 val applicationName by extra { (tasks["war"] as War).archiveFileName.get() }
 
 liberty {
-    server = ServerExtension("forecastServer")
-    server.apps = listOf(tasks["war"])
-    server.bootstrapProperties = mapOf("httpPort" to httpPort, "httpsPort" to httpsPort, "applicationName" to applicationName)
+    server = ServerExtension()
+    server.name = "forecastServer"
+    server.deploy.apps = listOf(tasks["war"])
+    server.bootstrapProperties = mapOf("applicationName" to applicationName).toProperties()
     server.configDirectory = file("src/main/liberty/config")
 }
 
