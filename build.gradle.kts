ffd3f9b6a9e8e14fef24a86a74000ea31f1ac470
realModusOperandi <liamwestby@icloud.com>
2022-06-07 21:28:04 -0500

    Update to newest OpenLiberty LTS

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.8.2")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.8.2")
 
-    libertyRuntime("io.openliberty", "openliberty-runtime", "[22.0.0.5,)")
+    libertyRuntime("io.openliberty", "openliberty-runtime", "[22.0.0.6,)")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
